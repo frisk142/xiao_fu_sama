38d513fc17f0a8a4f973a643bdc725ffc925a6e2
Frisk <2317216814@qq.com>
2026-04-08 16:12:52 +0800

    重新制作了一个新的前端聊天页面

--- Function/gui.py
+++ Function/gui.py
@@ -9,11 +9,10 @@ class XiaofuGui:
     def __init__(self):
         self.window = tb.Window(themename = "minty")
         self.window.title("小芙酱")
-        self.window.geometry("600x400")
+        self.window.geometry("750x650")
         self.window.resizable(width=False, height=False)
 
         # self.window.iconbitmap("xiao_fu.ico")
-
         # 显示聊天区域
         self.chat_area = scrolledtext.ScrolledText(
             self.window,
@@ -33,11 +32,47 @@ class XiaofuGui:
 
         # 底部输入区域
         self.input_frame = tb.Frame(self.window)
-        self.input_frame.pack(padx = 15, pady = (0,15), fill = tk.x)
+        self.input_frame.pack(padx = 15, pady = (0,15), fill = tk.X)
         self.input_entry = tb.Entry(self.input_frame, font=("微软雅黑",10))
-        self.input_entry.pack(size = tk.LEFT, fill = tk.x, expand = True, padx=(0,10))
+        self.input_entry.pack(side = tk.LEFT, fill = tk.X, expand = True, padx=(0,10))
+        self.input_entry.bind("<Return>",self.send_message)
         self.send_btn = tb.Button(self.input_frame,text = "发送", command = self.send_message, bootstyle= "primary")
         self.send_btn.pack(side = tk.RIGHT)
+
+        # 欢迎语
+        self.add_message("小芙酱","主人，我准备好啦，今天有什么可以帮助你的吗？", is_user = False)
+
+        self.window.mainloop()
+
+        # 设置添加语句模块
+    def add_message(self, sender, message, is_user = True):
+        self.chat_area.config(state = tk.NORMAL)
+        if is_user:
+            self.chat_area.insert(tk.END,f"你:{message}\n","user")
+        else:
+            self.chat_area.insert(tk.END,f"{sender}:{message}\n","bot")
+            self.chat_area.see(tk.END)
+            self.chat_area.config(state=tk.DISABLED)
+
+        # 设置发送消息模块
+    def send_message(self, event = None):
+        user_input = self.input_entry.get().strip()
+        if not user_input:
+            return
+        self.add_message("你",user_input,is_user = True)
+        self.input_entry.delete(0, tk.END)
+
+        def call_bot():
+            reply = chat_with_fu_jiang(user_input)
+            self.window.after(0,self.add_message, "小芙酱", reply, False)
+
+        threading.Thread(target=call_bot, daemon=True).start()
+
+if __name__ == "__main__":
+    XiaofuGui()
+
+
+
 
 
 
